19b087614bda6719759f9b3d9a95e2edf2bc657b
leonguyen <nam85it@gmail.com>
2022-11-23 16:18:59 +0700

    1 file modified

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'https://scjmall.vn', true);
+request.open('GET', ' https://online.namnguyen68.repl.co', true);
 
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
